20c251bcfd40a937e91c5cd472c2904e72bedd71
Mahima Gupta <guptamahima583@gmail.com>
2026-03-20 20:34:49 +0530

    editing navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -17,17 +17,13 @@ export default function Navbar() {
 
   return (
     <div className="sticky top-0 z-50 backdrop-blur-md bg-linear-to-r from-black via-[#001f2f] to-black border-b border-white/10 pl-2 pr-8 flex items-center justify-between h-30">
-
-      {/* 🔷 Logo */}
       <div className="flex items-center">
         <Image
           src={Logo}
           alt="projectLogo"
-          className="w-52 h-auto object-contain drop-shadow-[0_0_6px_rgba(0,212,255,0.4)]"
+          className="w-52 h-auto object-contain"
         />
       </div>
-
-      {/* 🔗 Nav Links */}
       <div className="flex items-center gap-6">
         {navLinks.map((link) => (
           <Link
@@ -37,16 +33,12 @@ export default function Navbar() {
               ${pathname === link.path ? "text-cyan-400" : "text-white/80 hover:text-cyan-300"}`}
           >
             {link.name}
-
-            {/* 🔥 Active underline */}
             {pathname === link.path && (
               <span className="absolute left-0 -bottom-1 w-full h-0.5 bg-cyan-400 rounded-full"></span>
             )}
           </Link>
         ))}
       </div>
-
-      {/* 🔘 Auth Buttons */}
       <div className="flex items-center gap-4">
         {isSignedIn ? (
           <>
