10897a734cf623f42c0cf75ac6a0b7942cea9308
Mahima Gupta <guptamahima583@gmail.com>
2026-03-21 22:37:01 +0530

    editing font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
 import Navbar from "./components/navbar";
 import "../../public/projectLogo.png";
 import { Toaster } from "sonner";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Lato, Lexend } from "next/font/google";
+
+const lato = Lato({
   subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  variable: "--font-lato",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lexend = Lexend({
   subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-lexend",
 });
 
 export const metadata: Metadata = {
@@ -28,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
+        <main className={`${lato.variable} ${lexend.variable} antialiased min-h-screen flex flex-col`}>
           <ClerkProvider>
             <Navbar />
             <div className="flex flex-col flex-1 justify-center items-center bg-background text-textColor w-full">
